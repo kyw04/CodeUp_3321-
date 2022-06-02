7f5e891925de54a89fed5289b0177277d12af482
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-06-02 19:51:43 +0900

    초기 커밋.

---
+++ CodeUp_3321 최고의 피자/소스.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	int dough_price, topping_price, dough_calorie, price, calorie, maximum;
+	cin >> n >> dough_price >> topping_price >> dough_calorie;
+	vector<int> topping_calorie(n);
+	for (auto& a : topping_calorie)
+		cin >> a;
+	sort(topping_calorie.begin(), topping_calorie.end(), greater<>());
+
+	price = dough_price;
+	calorie = dough_calorie;
+	maximum = calorie / price;
+
+	for (auto& a : topping_calorie)
+	{
+		price += topping_price;
+		calorie += a;
+		if (maximum < calorie / price)
+			maximum = calorie / price;
+	}
+
+	cout << maximum;
+	return 0;
+}
